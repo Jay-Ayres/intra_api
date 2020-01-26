fcc867d3659c90810a40311fb41edb65db2bc5e5
jayce ayres <jayceayres@jayces-MacBook-Pro.local>
2020-01-26 09:09:28 -0500

    timestamp ajust

--- src/database/migrations/20200114015241-create-posts.js
+++ src/database/migrations/20200114015241-create-posts.js
@@ -29,12 +29,10 @@ module.exports = {
       },
       created_at: {
         type: "TIMESTAMP",
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
         allowNull: false
       },
       updated_at: {
         type: "TIMESTAMP",
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
         allowNull: false
       }
     });
--- src/database/migrations/20200118210938-create-events_users.js
+++ src/database/migrations/20200118210938-create-events_users.js
@@ -23,12 +23,10 @@ module.exports = {
       },
       created_at: {
         type: "TIMESTAMP",
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
         allowNull: false
       },
       updated_at: {
         type: "TIMESTAMP",
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
         allowNull: false
       }
     });
