858812b6a76b1caed9c72545b744c9ae6e08c811
jayce ayres <jayceayres@jayces-MacBook-Pro.local>
2020-01-26 10:13:26 -0500

    database config

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 
 module.exports = {
   dialect: process.env.DB_CONNECTION,
-  // host: process.env.DB_HOST,
-  // username: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_DATABASE,
-  url: process.env.CLEARDB_DATABASE_URL,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  // url: process.env.CLEARDB_DATABASE_URL,
   define: {
     timestamps: true,
     underscored: true,
